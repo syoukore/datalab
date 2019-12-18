03ecd6b89040deaefb5d62553d61a26a54ece759
hchsiao <hchsiao@vlsilab.ee.ncku.edu.tw>
2019-12-18 20:11:04 +0800

    more implementation

--- packages/myma/date.py
+++ packages/myma/date.py
@@ -1,11 +1,12 @@
 import copy
 import datetime
+from dateutil.relativedelta import relativedelta
 class Date:
   def __init__(self, date):
     if type(date) is type(1):
       date = str(date)
-    if '/' in date:
-      y, m, d = date.split('/')
+    if '-' in date or '/' in date:
+      y, m, d = date.split('-') if '-' in date else date.split('/')
       assert len(y) == 3 or len(y) == 4 # taiwanese or western year
       y = int(y) if len(y) == 4 else int(y) + 1911
       date = str(y) + m + d
@@ -15,27 +16,26 @@ class Date:
     self.dd = int(date[6:8])
     self.date = datetime.datetime(self.yyyy, self.mm, self.dd)
 
-  def _datetime_op(self, other, op):
-    if type(other) is type(1):
-      other = datetime.timedelta(days=other)
+  def shift(self, amt, unit='day'):
+    if 'year' == unit:
+      delta = relativedelta(years=abs(amt))
+    elif 'month' == unit:
+      delta = relativedelta(months=abs(amt))
+    else:
+      delta = relativedelta(days=abs(amt))
     result = copy.copy(self)
-    result.date = op(self.date, other)
+    if amt < 0:
+      result.date = self.date - delta
+    else:
+      result.date = self.date + delta
     result.yyyy = result.date.year
     result.mm = result.date.month
     result.dd = result.date.day
     return result
 
-  def __add__(self, other):
-    return self._datetime_op(other, lambda x, y: x + y)
-
-  def __sub__(self, other):
-    return self._datetime_op(other, lambda x, y: x - y)
-
   def __str__(self):
-    return self.date.strftime('%Y%m%d')
+    return self.date.strftime('%Y-%m-%d')
 
   def __repr__(self):
-    return self.date.strftime('%Y%m%d')
+    return self.date.strftime('%Y-%m-%d')
 
-  def __int__(self):
-    return int(str(self))
--- packages/myma/valid_dates.py
+++ packages/myma/valid_dates.py
@@ -4,44 +4,47 @@ import datetime
 import sqlite3
 from sqlite3 import Error
 from myma.date import Date
+import time
 
+# TODO: phase
 def get_dates(target_date, N, stock_no, phase_shift='lag'):
   conn = None
   create_sql = "CREATE TABLE IF NOT EXISTS valid_dates (\
       date integer, stock_no integer, PRIMARY KEY (date, stock_no)\
   );"
   insert_sql = "INSERT INTO valid_dates(date, stock_no)\
-      VALUES(?, ?)"
+      VALUES(strftime('%s', ?), ?)"
   lagphase_sql = "SELECT *\
       FROM valid_dates\
-      WHERE stock_no = ? AND date < ?\
+      WHERE stock_no = ? AND date < date(?, 'unixepoch')\
       ORDER BY date DESC\
       LIMIT ?;"
-  zerophase_sql = "SELECT *\
+  zerophase_sql = "SELECT date(date, 'unixepoch')\
       FROM valid_dates\
       WHERE stock_no = ?\
-      ORDER BY Abs(date - ?) ASC\
+      ORDER BY Abs(date - strftime('%s', ?)) ASC\
       LIMIT ?;"
   
   try:
     conn = sqlite3.connect('cache/myma.db')
     c = conn.cursor()
     c.execute(create_sql)
-    db_max = next(c.execute("SELECT MAX(date) FROM valid_dates WHERE stock_no = ?", (stock_no,)))[0]
-    db_min = next(c.execute("SELECT MIN(date) FROM valid_dates WHERE stock_no = ?", (stock_no,)))[0]
+    db_max = next(c.execute("SELECT date(MAX(date), 'unixepoch') FROM valid_dates WHERE stock_no = ?", (stock_no,)))[0]
+    db_min = next(c.execute("SELECT date(MIN(date), 'unixepoch') FROM valid_dates WHERE stock_no = ?", (stock_no,)))[0]
     select_sql = zerophase_sql
-    res = [r[0] for r in c.execute(select_sql, (stock_no, int(target_date), N))]
+    res = [r[0] for r in c.execute(select_sql, (stock_no, str(target_date), N))]
   
+    # TODO: what if target in the future
+    shift_amt = 1#TODO: to prevent long suspension
     if db_max is None:
       fetch_date = target_date
     elif db_max in res:
-      fetch_date = Date(db_max) + 1
+      fetch_date = Date(db_max).shift(shift_amt, 'month')
     elif db_min in res:
-      fetch_date = Date(db_min) - 1 # TODO what if min = 190102 => 0102 shouldn't in retval, incremental month decrease (for long suspension)
+      fetch_date = Date(db_min).shift(-shift_amt, 'month') # TODO what if min = 190102 => 0102 shouldn't in retval
     else:
       fetch_date = None
     print(fetch_date, db_max, db_min, res)
-    # TODO FIXBUG: 190101 - 181231 should= 1
 
     if fetch_date is not None:
       retval = crawl_date(fetch_date, stock_no)
@@ -56,10 +59,20 @@ def get_dates(target_date, N, stock_no, phase_shift='lag'):
   return
   
 def crawl_date(date, stock_no):
-  date_s = str(date)
-  r = requests.get(
-      f'https://www.twse.com.tw/exchangeReport/STOCK_DAY?\
-          response=json&date={date_s}&stockNo={stock_no}')
+  date_s = str(date).replace('-', '')
+  while True: # TODO: do not try forever
+    try:
+      r = requests.get(
+          f'https://www.twse.com.tw/exchangeReport/STOCK_DAY?\
+              response=json&date={date_s}&stockNo={stock_no}')
+      break
+    except requests.exceptions.ConnectionError as e:
+      if 'Connection aborted' in str(e):
+        print('Connection error, waiting for retry...')
+        time.sleep(10)
+        continue
+      else:
+        assert False # TODO: handle
   page = r.text
   jsn = json.loads(page)
   dates = [Date(info[0]) for info in jsn['data']]
